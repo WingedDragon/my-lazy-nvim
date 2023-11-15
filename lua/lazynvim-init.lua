26246fcffae737bd71af559573444d0bfe924a90
dongzhenxing <dongzhenxing@mininglamp.com>
2023-11-15 16:57:17 +0800

    Init

--- lua/lazynvim-init.lua
+++ lua/lazynvim-init.lua
@@ -4,14 +4,41 @@ if not vim.loop.fs_stat(lazypath) then
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
+    "https://mirror.ghproxy.com/https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+    defaults = {
+        lazy = true
+    },
+    git = {
+        url_format = "https://mirror.ghproxy.com/https://github.com/%s.git"
+    },
+    ui = {
+        icons = {
+            cmd = "⌘",
+            config = "🛠",
+            event = "📅",
+            ft = "📂",
+            init = "⚙",
+            keys = "🗝",
+            plugin = "🔌",
+            runtime = "💻",
+            require = "🌙",
+            source = "📄",
+            start = "🚀",
+            task = "📌",
+            lazy = "💤 ",
+        },
+    }
+})
+
+require("basic")
+require("core")
 
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
---
+++ lua/plugins/view.lua
@@ -0,0 +1,66 @@
+return {
+	-- winbar
+	{
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons", -- optional dependency
+		},
+		config = function()
+			require("barbecue").setup({
+				create_autocmd = false, -- prevent barbecue from updating itself automatically
+				theme = vim.g.hardhacker_barbecue_theme,
+			})
+
+			vim.api.nvim_create_autocmd({
+				"WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+				"BufWinEnter",
+				"CursorHold",
+				"InsertLeave",
+
+				-- include this if you have set `show_modified` to `true`
+				"BufModifiedSet",
+			}, {
+				group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+				callback = function()
+					require("barbecue.ui").update()
+				end,
+			})
+		end,
+	},
+	-- Outline
+	{
+		"stevearc/aerial.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("aerial").setup({
+				lazy_load = false,
+				open_automatic = require("custom_opts").auto_open_outline,
+			})
+		end,
+	},
+	-- File explorer
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("config/neo-tree")()
+		end,
+	},
+	{
+		"stevearc/dressing.nvim",
+		opts = {},
+	},
+}
